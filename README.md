a162307b4191f4b6fe42a3ce92ff6198959f1485
agprados <aidagp@uma.es>
2017-01-17 13:24:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# emasa-gae
---
+++ src/com/emasagae/config/OfyHelper.java
@@ -0,0 +1,23 @@
+package com.emasagae.config;
+
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyFactory;
+import com.googlecode.objectify.ObjectifyService;
+
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletContextEvent;
+
+public class OfyHelper implements ServletContextListener {
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		//ObjectifyService.register(Pimiento.class);		
+	}
+
+}
