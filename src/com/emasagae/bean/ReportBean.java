3905d4a70c2998cb07c4e980ef12dd085532f274
rsa568422 <rsa568422@hotmail.com>
2017-01-18 16:57:01 +0100

    Epiezo a hacer el primer punto de lo basico
    ***************
    * Index.xhtml *
    ***************
    Utilizo el index que dejo Aida en el git y le agrego la tabla para los
    reports
    
    **************************
    * ObjetifyReportDAO.java *
    **************************
    Le estoy agregando metodo para busqueda por orden cronologico
    
    *******************
    * ReportBean.java *
    *******************
    Agrego variables provadas:
    	private List<Report> reports;
    	private ObjectifyReportDAO db;
    Metodos getter de reports
    Preparado metodo doVerAviso para posteriormente poder consultar un aviso

--- src/com/emasagae/bean/ReportBean.java
+++ src/com/emasagae/bean/ReportBean.java
@@ -1,14 +1,38 @@
 package com.emasagae.bean;
 
+import com.emasagae.dao.ObjectifyReportDAO;
+import com.emasagae.entity.Report;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
 import java.io.Serializable;
+import java.util.List;
 
 @ManagedBean
 @RequestScoped
 public class ReportBean implements Serializable{
+	
+	private List<Report> reports;
+	private ObjectifyReportDAO db;
 
 	private static final long serialVersionUID = 1L;
+	
+	public ReportBean() {
+    }
+    
+    @PostConstruct
+    public void init() {
+    	db = new ObjectifyReportDAO();
+    	reports = db.findAll();
+    }
 
+	public List<Report> getReports() {
+		return reports;
+	}
+	
+	public String doVerAviso(Report report) {
+		return "index";
+	}
 }
--- src/com/emasagae/dao/ObjectifyReportDAO.java
+++ src/com/emasagae/dao/ObjectifyReportDAO.java
@@ -1,7 +1,13 @@
 package com.emasagae.dao;
 
+import java.util.List;
+
 import com.emasagae.entity.Report;
 
 public class ObjectifyReportDAO extends ObjectifyGenericDAO<Report> implements IReportDAO {
-
+	
+	public List<Report> findAllSortedByCreationDate() {
+		return null;
+	}
+	
 }
