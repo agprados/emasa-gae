39d4c3084f792ddf6d6b210227b03ace2b4ec55a
Julio <Julio@192.168.1.88>
2017-01-18 22:21:12 +0100

    Arreglando cosas

--- src/com/emasagae/dao/ObjectifyReportDAO.java
+++ src/com/emasagae/dao/ObjectifyReportDAO.java
@@ -1,12 +1,10 @@
 package com.emasagae.dao;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import com.emasagae.entity.Report;
-import com.emasagae.entity.ReportUser;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -31,24 +29,16 @@ public class ObjectifyReportDAO extends ObjectifyGenericDAO<Report> implements I
 			String type = (String) e.getProperty("type");
 			String address = (String) e.getProperty("address");
 			Integer zip = new Integer((String) e.getProperty("zip"));
-			BigDecimal geolat = new BigDecimal((String) e.getProperty("geolat"));
-			BigDecimal geolng = new BigDecimal((String) e.getProperty("geolng"));
 			String description = (String) e.getProperty("description");
 			String priority = (String) e.getProperty("priority");
 			Date creationdate = new Date();//((String) e.getProperty("creationdate"));
 			Date startdate = new Date();//((String) e.getProperty("startdate"));
 			Date finishdate = new Date();//((String) e.getProperty("finishdate"));
-			Long idReportUser = new Long((String) e.getProperty("reportUser"));
-			ObjectifyReportUserDAO aux = new ObjectifyReportUserDAO();
-			ReportUser reportUser = aux.findById(idReportUser);
 			Report r = new Report(id, state, address, description, priority, creationdate);
 			r.setType(type);
 			r.setZip(zip);
-			r.setGeolat(geolat);
-			r.setGeolng(geolng);
 			r.setStartdate(startdate);
 			r.setFinishdate(finishdate);
-			r.setReportUser(reportUser);
 			reports.add(r);
 		}
 		
