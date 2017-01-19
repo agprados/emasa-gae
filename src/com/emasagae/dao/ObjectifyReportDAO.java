e057d5e49cefaa3e9b8af0fa3e4bff6fe3135ba2
rsa568422 <rsa568422@hotmail.com>
2017-01-19 15:34:34 +0100

    al carajo reset

--- src/com/emasagae/dao/ObjectifyReportDAO.java
+++ src/com/emasagae/dao/ObjectifyReportDAO.java
@@ -1,48 +1,15 @@
 package com.emasagae.dao;
 
-import java.util.ArrayList;
-import java.util.Date;
+import static com.googlecode.objectify.ObjectifyService.ofy;
 import java.util.List;
-
 import com.emasagae.entity.Report;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.Query;
 
 public class ObjectifyReportDAO extends ObjectifyGenericDAO<Report> implements IReportDAO {
 	
 	public List<Report> findAllSortedByCreationDate() {
 		
-		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
-		
-		Query query = new Query("Report").
-		addSort("creationdate",Query.SortDirection.DESCENDING);
-		
-		List<Entity> entities = ds.prepare(query).asList(FetchOptions.Builder.withDefaults());
-		
-		List<Report> reports = new ArrayList<Report>();
-		for (Entity e : entities) {
-			Long id = new Long((String) e.getProperty("id"));
-			String state = (String) e.getProperty("state");
-			String type = (String) e.getProperty("type");
-			String address = (String) e.getProperty("address");
-			Integer zip = new Integer((String) e.getProperty("zip"));
-			String description = (String) e.getProperty("description");
-			String priority = (String) e.getProperty("priority");
-			Date creationdate = new Date();//((String) e.getProperty("creationdate"));
-			Date startdate = new Date();//((String) e.getProperty("startdate"));
-			Date finishdate = new Date();//((String) e.getProperty("finishdate"));
-			Report r = new Report(id, state, address, description, priority, creationdate);
-			r.setType(type);
-			r.setZip(zip);
-			r.setStartdate(startdate);
-			r.setFinishdate(finishdate);
-			reports.add(r);
-		}
-		
-		return reports;
+		return ofy().load().type(Report.class).order("-creationdate").list();
+
 	}
 	
 }
